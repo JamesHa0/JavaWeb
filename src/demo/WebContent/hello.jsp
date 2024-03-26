e3c46b3e22c11ff87f3c6b484cfa25d253d6d98f
JamesHao <zhyuza@qq.com>
2024-03-26 19:26:21 +0800

    实验一

---
+++ src/demo/WebContent/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>A Simple JSP Page</title>
+</head>
+<body>
+<font color="#0000ff">
+<h3>Hello,World!+22023237</h3>
+The time now is:<%=new java.util.Date()%>
+		</font>
+</body>
+</html>
---
+++ src/demo/src/helloapp.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloapp", urlPatterns = { "/hello" })
+//java反射机制，直接通过url（/hello)找到这个类(helloapp)
+public class helloapp extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Hello, World!</title></head><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
